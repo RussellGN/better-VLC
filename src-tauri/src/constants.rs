162c44087e5a6acfb59f0f6be1072935c3a8c7c5
Russellg <dudurussell@gmail.com>
2025-02-26 14:07:49 +0200

    feat: setup backend module structure; define store name constant

---
+++ src-tauri/src/constants.rs
@@ -0,0 +1,1 @@
+pub const STORE_NAME: &str = "store.json";
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,7 @@
+mod commands;
+mod constants;
+mod structs;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
