02f5d7e0c1a9684faa0f77d02ec1e27ccfcd9de1
Russellg <dudurussell@gmail.com>
2025-02-26 12:58:32 +0200

    feat: add refresh function, error and loading states to video list; integrate home-videolist with videos hook

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,15 +1,20 @@
-import { sampleVideos } from "@/lib/sampleData";
 import { TvMinimalPlay } from "lucide-react";
 import VideoList from "./VideoList";
+import useVideos from "@/hooks/useVideos";
 
 export default function Home() {
+   const { videos, isLoading, error, refresh } = useVideos();
+
    return (
       <div>
          <VideoList
-            videos={sampleVideos}
+            videos={videos}
+            isLoading={isLoading}
+            error={error}
             icon={TvMinimalPlay}
             contentClassName=" max-h-[75vh] overflow-y-auto"
             title="All media"
+            refresh={refresh}
          />
       </div>
    );
--- src/routes/VideoList.tsx
+++ src/routes/VideoList.tsx
@@ -1,23 +1,45 @@
 import VideoThumbnail from "@/components/general/VideoThumbnail";
 import Section from "@/components/general/Section";
 import { Video } from "@/lib/types";
-import { LucideIcon } from "lucide-react";
+import { LucideIcon, RefreshCcw, TriangleAlert } from "lucide-react";
 import { cn } from "@/lib/utils";
+import Spinner from "@/components/general/Spinner";
+import { Button } from "@/components/ui/button";
 
 type propTypes = {
    title: string;
    icon: LucideIcon;
-   videos: Video[];
-   contentClassName: string;
+   videos: Video[] | undefined;
+   contentClassName?: string;
+   error?: string | undefined;
+   isLoading?: boolean;
+   refresh?: () => void;
 };
 
-export default function VideoList({ title, icon, videos, contentClassName }: propTypes) {
+export default function VideoList({ title, icon, videos, contentClassName, error, isLoading, refresh }: propTypes) {
    return (
       <Section icon={icon} title={title}>
-         <div className={cn("flex items-center flex-wrap gap-5", contentClassName)}>
-            {videos.map((vid) => (
-               <VideoThumbnail video={vid} key={vid.id} />
-            ))}
+         <div className="flex flex-col items-center justify-center text-center gap-3">
+            {error ? (
+               <>
+                  <TriangleAlert /> {error}
+               </>
+            ) : isLoading ? (
+               <>
+                  <Spinner /> Loading, please wait...
+               </>
+            ) : videos?.length === 0 ? (
+               <>
+                  0 videos found
+                  <Button onClick={refresh} variant="primary" size="icon">
+                     <RefreshCcw />
+                  </Button>
+               </>
+            ) : (
+               <div className={cn("w-full text-left flex items-center flex-wrap gap-5", contentClassName)}>
+                  {videos?.map((vid) => <VideoThumbnail video={vid} key={vid.id} />)}
+               </div>
+            )}
          </div>
       </Section>
    );
