e8e079940ab53e8a2342c624a4cc399cca1a6982
Russellg <dudurussell@gmail.com>
2025-02-26 12:57:01 +0200

    feat: implement 'videos' data and status hook

---
+++ src/hooks/useVideos.ts
@@ -0,0 +1,30 @@
+import { BACKEND_COMMANDS, VIDEOS_KEY } from "@/lib/constants";
+import { Video } from "@/lib/types";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { invoke } from "@tauri-apps/api/core";
+
+export default function useVideos() {
+   const {
+      data: videos,
+      error: getError,
+      isLoading,
+   } = useQuery<Video[], string>({
+      queryKey: VIDEOS_KEY,
+      queryFn: () => invoke<Video[]>(BACKEND_COMMANDS.getVideos),
+   });
+
+   const {
+      mutate: refresh,
+      error: refreshError,
+      isPending,
+   } = useMutation<unknown, string>({
+      mutationFn: () => invoke(BACKEND_COMMANDS.refreshVideos),
+   });
+
+   const e1 = getError ? " Error getting videos: " + getError : "";
+   const e2 = refreshError ? " Error getting videos: " + refreshError : "";
+
+   // console.log({ videos, getError, refreshError, isLoading, isPending, refresh });
+
+   return { videos, error: e1 + e2, isLoading: isLoading || isPending, refresh };
+}
---
+++ src/lib/constants.ts
@@ -0,0 +1,6 @@
+export const VIDEOS_KEY = ["videos"];
+
+export const BACKEND_COMMANDS = {
+   getVideos: "get_videos",
+   refreshVideos: "refresh_videos",
+};
