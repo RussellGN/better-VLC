fe478c8ffb688562dab8e5d3361cc9b2191be8d2
Russellg <dudurussell@gmail.com>
2025-02-27 14:46:52 +0200

    feat: configure video element

--- src/routes/Watch.tsx
+++ src/routes/Watch.tsx
@@ -2,16 +2,23 @@ import { useParams } from "react-router";
 
 export default function Watch() {
    const { "*": pathToVideo } = useParams();
-
    return (
       <div>
          <div className="relative bg-dark-dark border border-white/30">
-            <div className="flex gap-2 absolute top-0 left-0 bg-primary-dark shadow-lg   rounded-ee p-2">
+            <div className="flex gap-2 absolute z-10 text-sm top-0 left-0 bg-primary-dark shadow-lg   rounded-ee px-1">
                <p>Watch </p>
-               <p className="text-secondary-dark">{pathToVideo}</p>
+               <p className="text-secondary-dark w-[200px] overflow-hidden text-ellipsis text-nowrap">{pathToVideo}</p>
             </div>
 
-            <div className="h-[80vh] mx-auto  flex items-center justify-center ">Video Player</div>
+            <div className="h-[80vh] mx-auto  flex items-center justify-center ">
+               <video
+                  src={"file://" + pathToVideo}
+                  width={800}
+                  height={450}
+                  className="h-full w-auto "
+                  controls
+               ></video>
+            </div>
          </div>
 
          <div className="text-center p-5 flex items-center justify-center">playback controls</div>
