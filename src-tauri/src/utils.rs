86feab363a63641928e3838b298fc981f3c4d048
Russellg <dudurussell@gmail.com>
2025-02-27 09:58:55 +0200

    fix: handle read_dir errors by logging

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -8,16 +8,21 @@ pub fn walk_dir_and<F>(dir: &Path, callback: &mut F) -> crate::Result
 where
     F: FnMut(DirEntry) -> crate::Result,
 {
-    for entry in fs::read_dir(dir).map_err(|e| e.to_string())? {
-        let entry = entry.map_err(|e| e.to_string())?;
-        let path = entry.path();
-        if path.is_dir() {
-            log(format!("[walk_dir_and] recursing in {path:?}").as_str());
-            walk_dir_and(&path, callback)?;
-        } else {
-            log(format!("[walk_dir_and] calling callback on {path:?}").as_str());
-            callback(entry)?;
+    match fs::read_dir(dir) {
+        Ok(entries) => {
+            for entry in entries {
+                let entry = entry.map_err(|e| e.to_string())?;
+                let path = entry.path();
+                if path.is_dir() {
+                    log(format!("[walk_dir_and] recursing in {path:?}").as_str());
+                    walk_dir_and(&path, callback)?;
+                } else {
+                    log(format!("[walk_dir_and] calling callback on {path:?}").as_str());
+                    callback(entry)?;
+                }
+            }
         }
+        Err(e) => log(format!("[walk_dir_and] could not read {dir:?}. {e:?}",).as_str()),
     }
 
     Ok(())
