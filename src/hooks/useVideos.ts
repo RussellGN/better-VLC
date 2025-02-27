4beb3830915d10f7361f89324d3ec44267980e57
Russellg <dudurussell@gmail.com>
2025-02-27 11:37:05 +0200

    feat: only fetch videos after mount completes

--- src/hooks/useVideos.ts
+++ src/hooks/useVideos.ts
@@ -2,13 +2,16 @@ import { BACKEND_COMMANDS, VIDEOS_KEY } from "@/lib/constants";
 import { Video } from "@/lib/types";
 import { useMutation, useQuery } from "@tanstack/react-query";
 import { invoke } from "@tauri-apps/api/core";
+import { useEffect } from "react";
 
 export default function useVideos() {
    const {
       data: videos,
       error: getError,
       isLoading,
+      refetch: fetchVideos,
    } = useQuery<Video[], string>({
+      enabled: false,
       queryKey: VIDEOS_KEY,
       queryFn: () => invoke<Video[]>(BACKEND_COMMANDS.getVideos),
    });
@@ -20,6 +23,10 @@ export default function useVideos() {
    } = useMutation<unknown, string>({
       mutationFn: () => invoke(BACKEND_COMMANDS.loadAndSaveNewMedia),
    });
+
+   useEffect(() => {
+      fetchVideos();
+   }, []);
 
    const e1 = getError ? "| Error getting videos: " + getError + " |" : "";
    const e2 = refreshError ? "| Error getting videos: " + refreshError + " |" : "";
