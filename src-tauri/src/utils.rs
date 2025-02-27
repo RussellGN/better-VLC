6d6345cadd7f39bd202046a043309c0a526aff69
Russellg <dudurussell@gmail.com>
2025-02-27 08:36:37 +0200

    feat: create utils module and define dir-walk util

---
+++ src-tauri/src/utils.rs
@@ -0,0 +1,21 @@
+use std::{
+    fs::{self, DirEntry},
+    path::Path,
+};
+
+pub fn walk_dir_and<F>(dir: &Path, callback: &mut F) -> crate::Result
+where
+    F: FnMut(DirEntry) -> crate::Result,
+{
+    for entry in fs::read_dir(dir).map_err(|e| e.to_string())? {
+        let entry = entry.map_err(|e| e.to_string())?;
+        let path = entry.path();
+        if path.is_dir() {
+            walk_dir_and(&path, callback)?;
+        } else {
+            callback(entry)?;
+        }
+    }
+
+    Ok(())
+}
